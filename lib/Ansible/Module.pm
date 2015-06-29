916002a2b7c5b0fde8edc7a749da1168cc2c9ff6
Paul Bennett <paul.w.bennett@gmail.com>
2015-06-29 03:53:10 -0400

    docs

--- lib/Ansible/Module.pm
+++ lib/Ansible/Module.pm
@@ -83,128 +83,102 @@ Version 0.001
 
 =head1 SYNOPSIS
 
-    # In your MyAnsibleModule.pm
     use Ansible::Module;
     my $module = Ansible::Module->new(
         argument_spec => {
-            state     => { default='present', choices=['present', 'absent'] },
-            name      => { required=True },
-            enabled   => { required=True, choices=BOOLEANS },
-            something => { aliases=['whatever'] },
+            state     => { default => 'present', choices => ['present', 'absent'] },
+            name      => { required => True },
+            enabled   => { required => True, choices => BOOLEANS },
+            something => { aliases => ['whatever'] },
         },
         supports_check_mode => True,
     );
 
-    if ($module->{ check_mode }) {
-        $module->exit_json(changed => check_if_system_state_would_be_changed()),
+    if ($module->check_mode) {
+        $module->exit_json(changed => state_would_change());
     }
-
-=head1 DANGER
-
-Do B<NOT> C<print> or C<say> from inside your module. You'll lose API
-compatability, as the API writes back to the caller in pue JSON.
 
 =head1 DESCRIPTION
 
 The goal of the Ansible::Module distribution is to replicate Ansible's built-in
-Python ansibleModule() support in Perl, as fully and as compatibly as possible.
+Python ansible_Module() support in Perl, as fully and as compatibly as possible.
 
 =head1 CONSTRUCTORS
 
 =head2 new
 
-Create a new Net::IPAddress::Util object
+Create a new Ansible::Module object.
 
-=head1 MAGIC
+B<NOTE> that it is also possible (with careful shenanigans) to derive your own
+classes from Ansible::Module. If you come up with a sane use-case, let me know
+and I'll try to incorporate it into the next release.
 
-This module exports special the constants C<True>, C<False>, and C<BOOLEANS>.
+=head1 OVERLOADED BOOLEAN CONSTANTS
+
+B<aka When is a truth not a truth?>
+
+This module provides the special constants C<True>, C<False>, and C<BOOLEANS>.
 
 =over
 
 C<True> and C<False> can be supplied as return values, but they may also be
 used with C<==>, C<eq>, and C<=~> to check the values of incoming data as being
 "truthy" or "falsey" respectively, following rather more Pythonesque rules than
-Perl's: "y", "yes", "true", and 1 are the only truthy values, and "n", "no",
-"false", and 0 are the only falsy values. Other Perl "false" values such as undef
-or the empty string will not register as "falsey". Other Perl "true" values
-(including the infamous zero-but-true "0e0") will not register as "truthy".
+Perl's:
+
+=over
+
+"y", "yes", "true" (all case-insensitive), and 1 are the only truthy values, and
 
 =back
 
 =over
 
+"n", "no", "false" (all case-insensitive), and 0 are the only falsy values.
+
+=back
+
+Other Perl "false" values such as undef or the empty string will not register
+as "falsey". Other Perl "true" values (including the infamous zero-but-true
+"0e0") will not register as "truthy". Because of this, you are B<strongly
+urged> to pass around C<True> and C<False> instead of relying on the code to DWIM.
+
+=back
+
 C<BOOLEANS> is similar, except it overloads C<==>, C<eq>, and C<=~> to check
 whether the supplied value matches C<True> || C<False>. It is mainly intended
 to be used within the definition of argument specs.
 
-=back
+=head1 PerlIO::via::Ansible
 
-=head1 OBJECT METHODS
+Do B<NOT> C<print()>, C<warn()>, C<carp()>, C<say()> or in other any way write
+directly to STDOUT or STDERR from inside your module. You'll lose API compatability,
+as the API writes back to the caller in pure JSON.
 
-=head2 is_ipv4
+If you absolutely, positively must write raw unstructured data to your caller,
+or if you want to capture the output of some module or command that insists on
+logging to STDOUT or STDERR (and pass that through unaltered to your caller, you
+are B<strongly encouraged> to use the bundled write-only PerlIO::via::Ansible IO
+layer (see L<open> and L<perliol> for more details).
 
-Returns true if this object represents an IPv4 address.
+    # Oh, my! We need to call something that uses a standard Perl warn()
+    open my $olderr, '>&', *STDERR;
+    open my $fh, '>:via(Ansible)', 'keyname';
+    open STDERR, '>&', $fh;
+    warn "Why would someone do this to themselves?";
+    open STDERR, '>&', $olderr;
 
-=head2 ipv4
+This results in an additional key-value pair being added to the output JSON, e.g.
 
-Returns the dotted-quad representation of this object, or an error if it is
-not an IPv4 address, for instance '192.168.0.1'.
+    {
+        ...
+        "keyname": "Why would somone do this to themselves? at line 123"
+        ...
+    }
 
-=head2 as_n32
-
-Returns the "N32" representation of this object (that is, a 32-bit number in
-network order) if this object represents an IPv4 address, or an error if it
-does not.
-
-=head2 as_n128
-
-Returns the "N128" representation of this object (that is, a 128-bit number in
-network order).
-
-You may supply one optional argument. If this argument is true, the return
-value will be a Math::BigInt object (allowing quickish and easy math involving
-two such return values), otherwise (if it is false (the default)), then the N128
-number will be returned as a bare string. If your platform can handle math with
-unsigned 128-bit integers, or if you will not be doing math on the results,
-then I strongly recommend the latter (default / false) option for performance
-reasons. In the true-argument case, you're advised to stringify the Math::BigInt
-math results as soon as is practical for performance reasons -- Math::BigInt is
-not "CPU free".
-
-=head2 ipv6
-
-Returns the canonical IPv6 string representation of this object, for
-instance 'fe80::1234:5678:90ab' or '::ffff:192.168.0.1'.
-
-=head2 ipv6_expanded
-
-Returns the IPv6 string representation of this object, without compressing
-extraneous zeroes, for instance 'fe80:0000:0000:0000:0000:1234:5678:90ab'.
-
-=head2 normal_form
-
-Returns the value of this object as a zero-padded 32-digit hex string,
-without the leading '0x', suitable (for instance) for storage in a database,
-or for other purposes where easy, fast sorting is desirable, for instance
-'fe8000000000000000001234567890ab'.
-
-=head2 '""'
-
-=head2 str
-
-=head2 as_str
-
-=head2 as_string
-
-If this object is an IPv4 address, it stringifies to the result of C<ipv4>,
-else it stringifies to the result of C<ipv6>.
-
-=head1 INTERNAL FUNCTIONS
-
-=head2 ERROR
-
-Either confess()es or cluck()s the passed string based on the value of
-$Net::IPAddress::Util::DIE_ON_ERROR, and if possible returns undef.
+One exception to the "don't touch STDOUT/STDERR" rule is that calls to C<die()>
+are automatically piped through the above process and end up in the
+{ "error": ... } key-value of the returned JSON.
 
 =head1 LICENSE
 
