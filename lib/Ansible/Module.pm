d4102fe108a7168e659b9cc1a67f15131903108f
Paul Bennett <paul.w.bennett@gmail.com>
2015-07-14 17:08:41 -0400

    docs

--- lib/Ansible/Module.pm
+++ lib/Ansible/Module.pm
@@ -26,7 +26,7 @@ sub _finish ($$) {
 
 sub new {
     my $class = ref($_[0]) ? ref(shift(@_)) : shift(@_);
-    my ($args_ref) = @_;
+    my ($args_ref) = (@_ % 2) ? shift(@_) : { @_ };
     my $opt_ref = Ansible::Module::Utils->getopt($args_ref->{ argument_spec });
     my $self = bless { %$args_ref, %$opt_ref } => $class;
     $self->fail_json({ msg => $Ansible::Module::Utils::errstr }) if $Ansible::Module::Utils::errstr;
@@ -105,13 +105,85 @@ Python ansible_Module() support in Perl, as fully and as compatibly as possible.
 
 =head1 CONSTRUCTORS
 
-=head2 new
+=over
+
+=item new(%OPTIONS)
 
 Create a new Ansible::Module object.
 
 B<NOTE> that it is also possible (with careful shenanigans) to derive your own
 classes from Ansible::Module. If you come up with a sane use-case, let me know
-and I'll try to incorporate it into the next release.
+and I'll try to incorporate more-usable code into the next release.
+
+=back
+
+=head2 SUPPORTED new() OPTIONS
+
+=over
+
+=item argument_spec
+
+Hashref defining the arguments your module implementation will accept. Each key
+in the hashref is an argument name, and each value is a "Validation Hashref",
+about which see below.
+
+=back
+
+=over
+
+=item supports_check_mode
+
+Ansible::Module Boolean (not a Perl boolean, see below) declaring whether or
+not your module implementation supports "check" mode -- that is, whether it can
+check the state of the system without changing the state of the system, or in
+other words, whether it has a "pretend" mode. If you declare this to be C<True>,
+then
+
+=over
+
+you B<MUST> check whether your implementation has been launched in check mode
+
+you B<MUST NOT> alter the system in any palpable way
+
+you B<MUST> (after checking whether changes would have been made) exit setting
+the C<changed> flag to either
+
+=over
+
+C<True> - Changes are needed
+
+C<False> - Changes are not needed
+
+=back
+
+=back
+
+=back
+
+=head2 VALIDATION HASHREFS
+
+As noted above, each argument to your module implementation B<MUST> have an
+entry in the L<argument_spec> hashref. This entry B<MUST> be a hashref and B<MUST>
+conform to the following spec:
+
+=over
+
+B<EXACTLY ONE> of
+
+=over
+
+=item C<required> => C<True>
+
+Specifies that your module implementation does B<NOT> provide a sensible default
+for the argument. This may be because there are multiple possible sensible
+defaults, depending on context, or because of some lazier reason.
+
+=item C<default> => $some_value
+
+Specifies that your module implementation B<DOES> provide a sensible default
+for the argument.
+
+=back
 
 =head1 OVERLOADED BOOLEAN CONSTANTS
 
@@ -153,7 +225,7 @@ to be used within the definition of argument specs.
 
 Do B<NOT> C<print()>, C<warn()>, C<carp()>, C<say()> or in other any way write
 directly to STDOUT or STDERR from inside your module. You'll lose API compatability,
-as the API writes back to the caller in pure JSON.
+as the API writes back to the caller in pure JSON to STDOUT.
 
 If you absolutely, positively must write raw unstructured data to your caller,
 or if you want to capture the output of some module or command that insists on
